ea47fd1c1c0e83b8238bafcac81dd9fb6747a18c
uzair khan ghori <ukghori08@gmail.com>
2026-05-25 13:05:47 -0400

    desktop: adapt config for AMD hardware with LUKS encryption

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -13,16 +13,31 @@
   nc.nixos.nvidia.enable = true;
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
+    device = "/dev/disk/by-label/NIXROOT";
     fsType = "ext4";
   };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType = "vfat";
+  };
+
+  boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-partlabel/cryptroot";
+
   boot.initrd.availableKernelModules = [
+    "ahci"
     "xhci_pci"
-    "nvme"
     "usb_storage"
+    "usbhid"
     "sd_mod"
   ];
+
+  boot.kernelModules = [ "kvm-amd" ];
+
+  hardware.cpu.amd.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+
+  zramSwap.enable = true;
 
   users.users.${config.nc.user.name} = {
     isNormalUser = true;
