bf8fa4a3bcbf43f162528b6f035b47b1f87589b7
uzair khan ghori <ukghori08@gmail.com>
2026-06-17 15:23:21 -0400

    t3c: bump

--- packages/t3-code-nightly/default.nix
+++ packages/t3-code-nightly/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "0.0.27";
+  version = "0.0.28-nightly.20260617.578";
 in
 stdenvNoCC.mkDerivation {
   pname = "t3-code-nightly";
@@ -14,7 +14,7 @@ stdenvNoCC.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/pingdotgg/t3code/releases/download/v${version}/T3-Code-${version}-arm64.zip";
-    hash = "sha256-2teOphbCdl1mQHFvDUK+qVdBdwHD/9uu/lY4VmRf1hU=";
+    hash = "sha256-4G2JiHwOBOmZIbtUPtXRJUrs1tzb1WIMHOnqyAy9ssw=";
   };
 
   nativeBuildInputs = [ unzip ];
