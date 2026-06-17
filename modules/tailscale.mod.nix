8aac36ca4fb8cbb9b63c3be7112f1375a860611d
uzair khan ghori <ukghori08@gmail.com>
2026-06-17 14:18:41 -0400

    tailscale

--- modules/tailscale.mod.nix
+++ modules/tailscale.mod.nix
@@ -1,12 +1,7 @@
 {
-  flake.darwinModules.tailscale =
-    { lib, pkgs, ... }:
-    let
-      inherit (lib.lists) singleton;
-    in
-    {
-      environment.systemPackages = singleton pkgs.tailscale;
-    };
+  flake.darwinModules.tailscale = {
+    services.tailscale.enable = true;
+  };
 
   flake.nixosModules.tailscale = {
     services.tailscale.enable = true;
