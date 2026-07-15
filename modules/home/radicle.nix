2870cbb9925d77e851f86d2fcda3a953ab3753a3
uzair khan ghori <ukghori08@gmail.com>
2026-07-15 16:31:54 -0400

    radicle: move config and data home to ~/.radicle

--- modules/home/radicle.nix
+++ modules/home/radicle.nix
@@ -10,22 +10,26 @@ let
   inherit (lib.lists) singleton;
 
   user = config.nc.user;
-  radicleHome = "${config.home-manager.users.${user.name}.xdg.dataHome}/radicle";
 in
 {
   home-manager.users.${user.name} = {
     home.packages = singleton pkgs.radicle-node;
 
-    home.sessionVariables.RAD_HOME = radicleHome;
+    home.sessionVariables.RAD_HOME = "${
+      config.home-manager.users.${user.name}.home.homeDirectory
+    }/.radicle";
 
-    xdg.dataFile."radicle/config.json".text = toJSON { } {
-      publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
-      preferredSeeds = [
-        "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.xyz:8776"
-        "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.xyz:8776"
-      ];
+    home.file.".radicle/config.json" = {
+      force = true;
+      text = toJSON { } {
+        publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
+        preferredSeeds = [
+          "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
+          "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
+        ];
 
-      node.alias = user.handle;
+        node.alias = user.handle;
+      };
     };
   };
 }
