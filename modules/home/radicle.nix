9c1f600ea3e274f6932165b823a6bcbaa8797b45
uzair khan ghori <ukghori08@gmail.com>
2026-07-27 17:42:23 -0400

    home/radicle: wire secrets module and adopt xdg data paths

--- modules/home/radicle.nix
+++ modules/home/radicle.nix
@@ -6,30 +6,44 @@
 }:
 
 let
+  inherit (lib.attrsets) optionalAttrs;
   inherit (lib.generators) toJSON;
   inherit (lib.lists) singleton;
 
   user = config.nc.user;
 in
 {
-  home-manager.users.${user.name} = {
-    home.packages = singleton pkgs.radicle-node;
-
-    home.sessionVariables.RAD_HOME = "${
-      config.home-manager.users.${user.name}.home.homeDirectory
-    }/.radicle";
-
-    home.file.".radicle/config.json" = {
-      force = true;
-      text = toJSON { } {
-        publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
-        preferredSeeds = [
-          "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
-          "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
-        ];
-
-        node.alias = user.handle;
-      };
-    };
+  secrets.radicle = {
+    file = ./radicle.age;
+  }
+  // optionalAttrs pkgs.stdenv.isDarwin {
+    owner = config.system.primaryUser;
   };
+
+  home-manager.users.${user.name} =
+    { config, osConfig, ... }:
+    {
+      home.packages = singleton pkgs.radicle-node;
+
+      home.sessionVariables.RAD_HOME = "${config.xdg.dataHome}/radicle";
+
+      xdg.dataFile."radicle/config.json" = {
+        force = true;
+        text = toJSON { } {
+          publicExplorer = "https://radicle.network/nodes/$host/$rid$path";
+          preferredSeeds = [
+            "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
+            "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
+          ];
+
+          node.alias = user.handle;
+        };
+      };
+
+      xdg.dataFile."radicle/keys/radicle".source =
+        config.lib.file.mkOutOfStoreSymlink osConfig.secrets.radicle.path;
+      xdg.dataFile."radicle/keys/radicle.pub".text = ''
+        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzDlJy403zDuRlof2dJcMGxHz9XZwSMIJkb4a64Hs5Z
+      '';
+    };
 }
