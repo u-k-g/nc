00c2463022526e02588e6f16c6f5c30f55cd3e0a
uzair khan ghori <ukghori08@gmail.com>
2026-08-03 15:22:53 -0400

    paneru: switch virtual workspaces via window manager bindings

--- modules/darwin/karabiner.nix
+++ modules/darwin/karabiner.nix
@@ -1,14 +1,11 @@
 {
   config,
-  lib,
-  pkgs,
   ...
 }:
 
 let
   user = config.nc.user;
   dotfiles = ../../dotfiles;
-  fastWorkspaceSwitch = pkgs.callPackage ../../packages/fast-workspace-switch { };
 in
 {
   system.activationScripts.preActivation.text = ''
@@ -20,8 +17,6 @@ in
   '';
 
   home-manager.users.${user.name} = {
-    home.packages = lib.lists.singleton fastWorkspaceSwitch;
-
     xdg.configFile."karabiner/karabiner.json".source = dotfiles + /config/karabiner/karabiner.json;
   };
 }
--- modules/darwin/paneru.nix
+++ modules/darwin/paneru.nix
@@ -53,6 +53,26 @@ in
         };
 
         bindings = {
+          window_virtualnum_1 = "alt - 1";
+          window_virtualnum_2 = "alt - 2";
+          window_virtualnum_3 = "alt - 3";
+          window_virtualnum_4 = "alt - 4";
+          window_virtualnum_5 = "alt - 5";
+          window_virtualnum_6 = "alt - 6";
+          window_virtualnum_7 = "alt - 7";
+          window_virtualnum_8 = "alt - 8";
+          window_virtualnum_9 = "alt - 9";
+
+          window_virtualmovenum_1 = "alt + shift - 1";
+          window_virtualmovenum_2 = "alt + shift - 2";
+          window_virtualmovenum_3 = "alt + shift - 3";
+          window_virtualmovenum_4 = "alt + shift - 4";
+          window_virtualmovenum_5 = "alt + shift - 5";
+          window_virtualmovenum_6 = "alt + shift - 6";
+          window_virtualmovenum_7 = "alt + shift - 7";
+          window_virtualmovenum_8 = "alt + shift - 8";
+          window_virtualmovenum_9 = "alt + shift - 9";
+
           window_focus_west = "alt - h";
           window_focus_south = "alt + cmd - j";
           window_focus_north = "alt + cmd - k";
