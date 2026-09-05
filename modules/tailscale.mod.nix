fad855de1002abaee9a5a953dcd7948774ce6c4c
uzair khan ghori <ukghori08@gmail.com>
2026-09-05 01:53:29 -0400

    tailscale: open member SSH, HTTPS, and 3773 access and set launchd PATH

--- modules/tailscale.mod.nix
+++ modules/tailscale.mod.nix
@@ -1,10 +1,15 @@
 { lib, ... }:
 let
   inherit (lib.lists) singleton;
+  inherit (lib.strings) replaceStrings;
 in
 {
-  flake.darwinModules.tailscale = {
+  flake.darwinModules.tailscale = { config, ... }: {
     homebrew.casks = singleton "tailscale-app";
+
+    launchd.user.envVariables.PATH =
+      replaceStrings [ "$HOME" "$USER" ] [ config.nc.user.homeDirectory config.nc.user.name ]
+        config.environment.systemPath;
   };
 
   flake.nixosModules.tailscale = {
