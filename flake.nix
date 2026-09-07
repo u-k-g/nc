c3054a3f9643de49da4d5e7ced969224dca339e9
uzair khan ghori <ukghori08@gmail.com>
2026-09-07 08:55:16 -0400

    Revert "homebrew: add freecad tap with dependencies and XDG trust-store env"
    This reverts commit f8cd157782b945b6d1b147553f3b2cda30d912d6.

--- flake.nix
+++ flake.nix
@@ -105,11 +105,6 @@
       flake = false;
     };
 
-    homebrew-freecad = {
-      url = "github:FreeCAD/homebrew-freecad";
-      flake = false;
-    };
-
     homebrew-osx-cross-arm = {
       url = "github:osx-cross/homebrew-arm";
       flake = false;
